92a92317a873cd72a390cab03f976b3e5273cf92
mahir-hs <mahirhasan333@gmail.com>
2025-03-15 17:43:00 +0600

    feat : added re routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RedirectComponent } from './redirect/redirect.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
     },
+    {
+        path: ':shortUrl',
+        component: RedirectComponent
+    } 
 
 ];
